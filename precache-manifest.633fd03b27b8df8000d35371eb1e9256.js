c746fa25f8c47e3c42541303f9fdf750ffefc6d8
David Wahrhaftig <davidw2009@hotmail.com>
2020-11-25 19:31:55 -0500

    Updates

---
+++ precache-manifest.633fd03b27b8df8000d35371eb1e9256.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b730ef19e3db1cd837f6f59ca56333f",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "8e64e55c1d3e5e762344",
+    "url": "/burger-builder/static/css/3.128ec37a.chunk.css"
+  },
+  {
+    "revision": "add86bd41eaf47f15139",
+    "url": "/burger-builder/static/css/4.9c2d93c4.chunk.css"
+  },
+  {
+    "revision": "0eecdd7f473d4336258e",
+    "url": "/burger-builder/static/css/5.1d94a73e.chunk.css"
+  },
+  {
+    "revision": "7033bb7fc2f493e2e8fa",
+    "url": "/burger-builder/static/css/6.42b5af21.chunk.css"
+  },
+  {
+    "revision": "093959826caccdddd36d",
+    "url": "/burger-builder/static/css/main.30fcf205.chunk.css"
+  },
+  {
+    "revision": "1f929bc5a740defe70fc",
+    "url": "/burger-builder/static/js/2.45bd7818.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.45bd7818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e64e55c1d3e5e762344",
+    "url": "/burger-builder/static/js/3.da93b752.chunk.js"
+  },
+  {
+    "revision": "add86bd41eaf47f15139",
+    "url": "/burger-builder/static/js/4.09c40396.chunk.js"
+  },
+  {
+    "revision": "0eecdd7f473d4336258e",
+    "url": "/burger-builder/static/js/5.e2647604.chunk.js"
+  },
+  {
+    "revision": "7033bb7fc2f493e2e8fa",
+    "url": "/burger-builder/static/js/6.115dc502.chunk.js"
+  },
+  {
+    "revision": "619341f727c17fb0c4c5",
+    "url": "/burger-builder/static/js/7.dce92499.chunk.js"
+  },
+  {
+    "revision": "093959826caccdddd36d",
+    "url": "/burger-builder/static/js/main.743bd057.chunk.js"
+  },
+  {
+    "revision": "d93a731893bbc0305ded",
+    "url": "/burger-builder/static/js/runtime-main.cd0b054d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
